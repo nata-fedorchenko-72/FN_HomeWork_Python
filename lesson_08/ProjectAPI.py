66a24bc95094cb82644d7286def1c0d17ebc9fba
Федорченко <nata.fedorchenko.72@gmail.com>
2025-03-26 20:18:05 +0300

    new7_branch

--- lesson_08/ProjectAPI.py
+++ lesson_08/ProjectAPI.py
@@ -6,53 +6,42 @@ class ProjectApi:
     def __init__(self, url):
         self.url = url
 
-    def get_auth(self, login="nata.fedorchenko.72@gmail.com", password="Nata><fedoR~1972"):
-        accounе = {
-            "login": login,
-            "password": password
-            }
-        resp = requests.post(self.url+"/auth/companies", json=accounе)
-        return resp.json()["content"][0]["id"]
-    
-    def get_key(self, login="nata.fedorchenko.72@gmail.com", password="Nata><fedoR~1972"):
-        response_key = {
-            "login": login,
-            "password": password,
-            "companyId": "6ae1affa-8587-472d-b688-716660196adc"
-            }
-        resp = requests.post(self.url+"/auth/keys", json=response_key)
-        return resp.json()["key"]
-    
-    def get_list_project(self):
+    def get_list_project(self, key):
         my_headers = {
-            'Authorization': f'Bearer {self.get_key()}'
+            'Authorization': f'Bearer {key}'
             }
         resp = requests.get(self.url+"/projects", headers=my_headers)
-        return resp.json()
-    
-    def get_project(self, id):
-        resp = requests.get(self.url+"/company/"+str(id))
-        return resp.json()
+        return resp
 
-    def creat_project(self, name, user):
+    def get_project(self, id, key):
+        my_headers = {
+            'Authorization': f'Bearer {key}'
+            }
+        resp = requests.get(self.url+"/company/"+str(id), headers=my_headers)
+        return resp
+
+    def creat_project(self, name, user, key):
         project = {
             "title": name,
             "users": user
             }
         my_headers = {
-            'Authorization': f'Bearer {self.get_key()}'
+            'Authorization': f'Bearer {key}'
             }
         resp = requests.post(
-            self.url + "/projects", json=project, headers=my_headers)
-        return resp.json()
-    
-    def modified_project(self, new_name, new_id):
+            self.url + "/projects", json=project, headers=my_headers
+            )
+        return resp
+
+    def modified_project(self, new_name, new_id, key):
         project = {
             "title": new_name
             }
         my_headers = {
-            'Authorization': f'Bearer {self.get_key()}'
+            'Authorization': f'Bearer {key}'
             }
         resp = requests.put(
-            self.url+ "/projects/"+ str(new_id), json=project, headers=my_headers)
-        return resp.json()
+            self.url + "/projects/" + str(new_id),
+            json=project, headers=my_headers
+            )
+        return resp
--- lesson_08/test_to_progects_section.py
+++ lesson_08/test_to_progects_section.py
@@ -1,37 +1,51 @@
 from ProjectAPI import ProjectApi
 
 url = ProjectApi("https://ru.yougile.com/api-v2")
+key = "tnE4FaqWGuFipQbwwiWHEenkHkzbQdSDfVJczvOdiJDihDzs1DUjeSzsoniyRnHy"
 
 
 def test_create_progect_positive():
-    my_list = url.get_list_project()
-    num_project = my_list['paging']['count']
-    list_orig = (num_project)
+    title = "Креатив"
+    user = {"bebdab3a-b10e-42c5-b146-ce022fcd5d1d": "admin"}
+    result = url.creat_project(title, user, key)
+    assert result.status_code == 201
 
+
+def test_get_projects_positive():
+    result = url.get_list_project(key)
+    assert result.status_code == 200
+    assert result.json()['content'][-1]['title'] == 'Креатив'
+
+
+def test_get_projects_negative():
+    result = url.get_list_project(key=None)
+    assert result.status_code == 401
+
+
+def test_create_project_negative():
+    title = ""
+    user = {"bebdab3a-b10e-42c5-b146-ce022fcd5d1d": "admin"}
+    result = url.creat_project(title, user, key)
+    assert result.status_code == 400
+
+
+def test_modified_project_positive():
     name = "Креатив"
     user = {"bebdab3a-b10e-42c5-b146-ce022fcd5d1d": "admin"}
-    result = url.creat_project(name, user)
-    new_id = result["id"]
-
-    my_list = url.get_list_project()
-    num_project = my_list['paging']['count']
-    list_new = (num_project)
-
-    new_company = url.get_project(new_id)
-
-    assert list_new - list_orig == 1
-    assert my_list[-1]["name"] == name
-    assert my_list['title'][-1]["id"] == new_id
-
-def test_modified_project():
-    name = "Креатив"
-    user = {"bebdab3a-b10e-42c5-b146-ce022fcd5d1d": "admin"}
-    result = url.creat_project(name, user)
-    new_id = result["id"]
+    result = url.creat_project(name, user, key)
+    new_id = result.json()["id"]
 
     new_name = "Креативный проект"
-    modif_project = url.modified_project(new_name, new_id)
+    url.modified_project(new_name, new_id, key)
+    assert result.status_code == 201
 
-    assert modif_project[-1]["name"] == new_name
-    
 
+def test_modified_project_negative():
+    name = "Креатив"
+    user = {"bebdab3a-b10e-42c5-b146-ce022fcd5d1d": "admin"}
+    result = url.creat_project(name, user, key)
+    result.json()["id"]
+
+    new_name = "Креативный проект"
+    modif_project = url.modified_project(new_name, "", key)
+    assert modif_project.status_code == 404
